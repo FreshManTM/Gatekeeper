f82b0bf6f11443b00c703acc97c2171836e96f64
TME <mtaran0238@gmail.com>
2024-08-05 19:35:48 +0300

    Small bug fixes

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -20,7 +20,6 @@ public class GameManager : MonoBehaviour
         Instance = this;
         DontDestroyOnLoad(gameObject);
         LoadInfo();
-        Coins = 1000;
     }
 
     public void LoadLevel(int levelNumber)
